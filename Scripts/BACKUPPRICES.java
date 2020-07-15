e22a23342e31e010724516d4f190282c4aeabfc8
KarimChaouch <66303315+KarimChaouch@users.noreply.github.com>
2020-07-15 09:55:18 +0200

    Revert "Revert "Move files to scripts""
    This reverts commit 4be98f71e4563b30f7b588b0c58d99131b3c4cc5.

---
+++ Scripts/BACKUPPRICES.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BACKUPPRICES {
+
+	public static void main(String[] args) throws IOException {
+
+		String PathToZone = "C:\\Users\\karim.chaouch\\Desktop\\Workspace\\PricePlan\\Zone.txt";
+		String PathToRules = "C:\\Users\\karim.chaouch\\Desktop\\Workspace\\PricePlan\\Rules.csv";
+		String PathToPrefix = "C:\\Users\\karim.chaouch\\Desktop\\Workspace\\PricePlan\\Prefix.csv";
+		String PathToTranslation = "C:\\Users\\karim.chaouch\\Desktop\\Workspace\\PricePlan\\Translation.csv";
+		String PathToPrice = "C:\\Users\\karim.chaouch\\Desktop\\Workspace\\PricePlan\\Price.csv";
+
+		String zone;
+		String rules;
+		String prefix;
+		String translation;
+		String price;
+
+		boolean rulesB = true;
+		boolean prefixB = true;
+		boolean translationB = true;
+		boolean priceB = true;
+
+		BufferedReader zoneReader = new BufferedReader(new FileReader(PathToZone));
+
+		while ((zone = zoneReader.readLine()) != null) {
+			String result = "--";
+			BufferedReader rulesReader = new BufferedReader(new FileReader(PathToRules));
+			while ((rules = rulesReader.readLine()) != null) {
+				String[] data1 = rules.split(";");
+
+				if (data1[0].equals(zone)) {
+
+//					rulesB = false;
+					BufferedReader prefixReader = new BufferedReader(new FileReader(PathToPrefix));
+					while ((prefix = prefixReader.readLine()) != null) {
+						String[] data2 = prefix.split(";");
+						if (data2[0].equals(data1[1])) {
+//							System.out.println(data2[0] + "  " + data2[1]);
+
+//							prefixB = false;
+							BufferedReader translationReader = new BufferedReader(new FileReader(PathToTranslation));
+							while ((translation = translationReader.readLine()) != null) {
+								String[] data3 = translation.split(";");
+
+								if (data3[0].equals(data2[1].toUpperCase())) {
+
+//									translationB = false;
+									BufferedReader priceReader = new BufferedReader(new FileReader(PathToPrice));
+									while ((price = priceReader.readLine()) != null) {
+										String[] data4 = price.split(";");
+										if (data4[0].equals(data3[1])) {
+
+//											priceB = false;
+											result = data4[1];
+											System.out.println(zone + "  " + result);
+
+										}
+
+									}
+									priceReader.close();
+
+								}
+
+							}
+							translationReader.close();
+						}
+
+					}
+
+					prefixReader.close();
+
+				}
+
+			}
+			rulesReader.close();
+
+		}
+
+		zoneReader.close();
+
+	}
+}
